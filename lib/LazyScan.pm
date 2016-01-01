16acfbbd6727c4568d3bff47993778d97dc36bfe
Nick Booker <nmb@nickbooker.uk>
2016-01-01 15:12:45 +0000

    Refactor batch listing into library

--- lib/LazyScan.pm
+++ lib/LazyScan.pm
@@ -2,14 +2,24 @@ package LazyScan;
 
 use strict;
 use warnings;
+use File::Spec;
+use File::Glob qw/:bsd_glob/;
 
 use Exporter::Easy (
-    OK => [qw/scandir/],
+    OK => [qw/scandir listbatch/],
 );
 
 
 sub scandir {
     $ENV{SCANHOME} or File::Spec->catfile($ENV{HOME}, 'scan')
+}
+
+sub listbatch {
+    my ($basedir, $batchnum) = @_;
+    my $pattern = File::Spec->catfile(
+        $basedir, sprintf('b*%d_p*.{png,pnm}', $batchnum));
+    my @match = bsd_glob($pattern, GLOB_CSH);
+
 }
 
 1;
